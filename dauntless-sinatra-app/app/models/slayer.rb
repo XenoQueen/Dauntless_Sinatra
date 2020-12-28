3907cd2c94bc5e829b6188ffc126bb6d11c1c2a2
Maika <maika@Amys-MBP.lan>
2020-12-28 15:42:14 -0500

    Created new migration create_slayers

---
+++ dauntless-sinatra-app/app/models/slayer.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base[5.2]
+    has_secure_password
+end
---
+++ dauntless-sinatra-app/db/migrate/20201228203818_create_slayers.rb
@@ -0,0 +1,9 @@
+class CreateSlayers < ActiveRecord::Migration
+  def change
+    create_table :slayers do |t|
+      t.string :name
+      t.string :username
+      t.string :password_digest
+    end
+  end
+end
