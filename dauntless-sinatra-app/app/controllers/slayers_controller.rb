f1258dceebfd59e551e3fbac594f10fea28ddd34
Maika <maika@Amys-MBP.lan>
2020-12-30 14:14:26 -0500

    add login.erb

--- dauntless-sinatra-app/app/controllers/slayers_controller.rb
+++ dauntless-sinatra-app/app/controllers/slayers_controller.rb
@@ -3,9 +3,22 @@ class SlayersController < ApplicationController
         erb :login
     end
 
+    post '/login' do
+        @slayer = Slayer.find_by(username: params[:username])
+        if @slayer.authenticate(params[:password])
+            session[:slayer_id] = @slayer.id
+            puts session
+            redirect "slayers/#{@slayer.id}"
+        else
+
+        end
+    end
+
     get '/signup' do
 
     end
 
-
+    get '/slayers/:id' do
+        "Welcome Slayer!"
+    end
 end
