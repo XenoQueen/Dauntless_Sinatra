ae0e745062140958936a389bfbbf0172196bfc6e
Maika <maika@Amys-MBP.lan>
2020-12-31 17:35:37 -0500

    Correct name issue with behemoths

--- dauntless-sinatra-app/app/controllers/behemoth_entries_controller.rb
+++
@@ -1,9 +0,0 @@
-class BehemothEntriesController < ApplicationController
-    get '/behemoth_entries/new' do
-        erb :'/behemoth_entries/new'
-    end
-
-    post '/behemoth_entries' do
-
-    end
-end
