f3036117ac35cff1ab3a29cdaed4a9a9bff66a90
Maika <maika@Amys-MBP.lan>
2020-12-31 16:35:18 -0500

    Add behemoth_entries_controller with get and post routes

---
+++ dauntless-sinatra-app/app/controllers/behemoth_entries_controller.rb
@@ -0,0 +1,9 @@
+class BehemothEntriesController < ApplicationController
+    get '/behemoth_entries/new' do
+        erb :'/behemoth_entries/new'
+    end
+
+    post '/behemoth_entries' do
+
+    end
+end
